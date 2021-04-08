c8523024ffcb484b05fb344d71126937fcfb3e89
Elyo <elyo.idesi@gmail.com>
2021-04-08 12:36:30 -0500

    updates auto made when build project

--- dist/location.service.js
+++ dist/location.service.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var endpoint = "locations";
 exports.default = (function (axios) {
     return {
-        create: function (data) {
-            return axios.post(endpoint, data);
+        create: function (params) {
+            return axios.post(endpoint, params);
         },
         getAllLocations: function (params) {
             return axios.get(endpoint, { params: params });
