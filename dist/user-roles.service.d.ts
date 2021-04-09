064bfefb90c8813a8ba74da3d40863b3a5e09520
Marisol <Marisol.isic96@gmail.com>
2021-04-09 14:45:29 -0500

    files not required deleted

--- dist/user-roles.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosInstance } from "axios";
-import { User } from "./types/User";
-declare const _default: (axios: AxiosInstance) => {
-    getAllUsersRoles: (params: User) => Promise<import("axios").AxiosResponse<any>>;
-};
-export default _default;
