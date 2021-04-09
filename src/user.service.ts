518bfcda6a45b52bd8e8e16b41fdcd4c8e865618
Marisol <Marisol.isic96@gmail.com>
2021-04-09 14:11:17 -0500

    the service name changed

---
+++ src/user.service.ts
@@ -0,0 +1,13 @@
+import{AxiosInstance} from "axios";
+import{User} from "./types/User";
+
+const endpointRoles = "roles";
+const endpointUsers = "users";
+
+export default (axios: AxiosInstance) => {
+    return{
+        getAllUsersRoles: (params: User) =>{
+            return axios.get(endpointRoles, {params});
+        }
+    }
+}
