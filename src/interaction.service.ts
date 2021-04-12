fd85d6159776db8288a5c82ab69095c03d585550
Elyo <elyo.idesi@gmail.com>
2021-04-12 11:48:08 -0500

    getInteractionsById method created

--- src/interaction.service.ts
+++ src/interaction.service.ts
@@ -1,14 +1,17 @@
 import { AxiosInstance } from "axios";
 import { Interactions, InteractionParams } from "./types/interactions";
 
-const endpoint = 'locations/';
+const endpoint = 'locations';
 //"TG9jYXRpb246MTI2NzE5" id del ejemplo del api
 //"TWVkaWFGaWxlOjEzMDMzMg==" id de locations
 
 export default (axios: AxiosInstance) => {
   return {
     getInteractions: (locationId:any, params: InteractionParams) => {
-      return axios.get(endpoint+locationId+"/reviews", {params});
+      return axios.get(`${endpoint}/${locationId}/reviews`, {params});
     },
+    getInteractionById(params:Interactions){
+      return axios.get(`${endpoint}/reviewDetails`,{params});
+    }
   };
 };
