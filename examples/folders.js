ce409869fcaf149b46cb8c3e6fd4f5cf4d812015
Elyo <elyo.idesi@gmail.com>
2021-04-15 12:02:04 -0500

    example to add location created

--- examples/folders.js
+++ examples/folders.js
@@ -1,3 +1,25 @@
 const synup = require("../dist/index")(process.env.SYNUP_KEY);
 
 const { Folders } = synup;
+
+let addLocations ={ 
+
+    "input":{  
+      "name":"Acme",
+      "locationIds":[  
+         "TG9jYXRpb246MTY4NjE=",
+         "TG9jYXRpb246MTY4NjA=",
+         "TG9jYXRpb246MTY4NDY="
+      ]
+   }
+ }
+
+Folders.addLocations(addLocations)
+.then((response) => {
+    console.log(JSON.stringify(response.data));
+    console.log("SUCCESS");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("FAILURE");
+  });
