7f711daf28ea203eecc5f0baeb18b7093f6fe34e
Elyo <elyo.idesi@gmail.com>
2021-04-15 12:01:48 -0500

    service to add location to a folder created

--- src/folders.service.ts
+++ src/folders.service.ts
@@ -1,12 +1,12 @@
 import { AxiosInstance } from "axios";
-import { AddLocations } from "./types/folders";
+import { AddLocationsPropierties } from "./types/folders";
 
 const endpoint = 'locations';
 
 export default (axios: AxiosInstance) => {
   return {
-    addLocations(params:AddLocations){
-        return axios.post(`${endpoint}/locations/folders`, params);
+    addLocations(params:AddLocationsPropierties){
+        return axios.post(`${endpoint}/folders`, params);
     }
   };
 };
