d5193379ca61ebe925b889e8f465971c9ebfe519
Elyo <elyo.idesi@gmail.com>
2021-04-15 19:08:00 -0500

    method in service to add location to a tag created

--- src/tags.service.ts
+++ src/tags.service.ts
@@ -1,9 +1,12 @@
 import { AxiosInstance } from "axios";
+import { AddLocations } from "./types/folders";
 
-const endpoint = "locations";
+const endpoint = "locations/tags";
 
 export default (axios: AxiosInstance) => {
   return {
-      
+    addLocations:(params:AddLocations)=>{
+        return axios.post(`${endpoint}`,params);
+    }
   }
 }
