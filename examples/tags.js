06df6e5fbac00332c56c71821d231eb128da3a00
Elyo <elyo.idesi@gmail.com>
2021-04-15 19:22:10 -0500

    example to remove location created

--- examples/tags.js
+++ examples/tags.js
@@ -2,15 +2,25 @@ const synup = require("../dist/index")(process.env.SYNUP_KEY);
 
 const { Tags } = synup;
 
-let addLocation={
+let tagsTest={
     "input":{
         "locationId":"TG9jYXRpb246MTY4NjA=",
         "tag":"New"
     }
 }
 
-Tags.addLocations(addLocation)
+Tags.addLocations(tagsTest)
 .then((response) => {
+    console.log(JSON.stringify(response.data));
+    console.log("SUCCESS");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("FAILURE");
+  });
+
+  Tags.removeLocations(tagsTest)
+  .then((response) => {
     console.log(JSON.stringify(response.data));
     console.log("SUCCESS");
   })
