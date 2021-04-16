7dbeab14ac555aa6b8c25faf98460d2d473507b8
Elyo <elyo.idesi@gmail.com>
2021-04-15 19:08:33 -0500

    tags example to add location created

--- examples/tags.js
+++ examples/tags.js
@@ -2,3 +2,19 @@ const synup = require("../dist/index")(process.env.SYNUP_KEY);
 
 const { Tags } = synup;
 
+let addLocation={
+    "input":{
+        "locationId":"TG9jYXRpb246MTY4NjA=",
+        "tag":"New"
+    }
+}
+
+Tags.addLocations(addLocation)
+.then((response) => {
+    console.log(JSON.stringify(response.data));
+    console.log("SUCCESS");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("FAILURE");
+  });
