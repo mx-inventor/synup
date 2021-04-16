e8aff90ee146ad3183af978cb5edf5bd4ba9cd62
Elyo <elyo.idesi@gmail.com>
2021-04-15 19:21:51 -0500

    remove locations method created in service

--- src/tags.service.ts
+++ src/tags.service.ts
@@ -1,12 +1,15 @@
 import { AxiosInstance } from "axios";
-import { AddLocations } from "./types/folders";
+import { LocationTag } from "./types/tags";
 
 const endpoint = "locations/tags";
 
 export default (axios: AxiosInstance) => {
   return {
-    addLocations:(params:AddLocations)=>{
+    addLocations:(params:LocationTag)=>{
         return axios.post(`${endpoint}`,params);
+    },
+    removeLocations:(params:LocationTag)=>{
+        return axios.post(`${endpoint}/remove`,params);
     }
   }
 }
