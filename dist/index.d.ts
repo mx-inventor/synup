f3565e05cbb88c1b8ab2563a6691cd5aa4de99f7
provisitrcebackend <provisitrcebackend@gmail.com>
2021-04-16 10:20:03 -0500

    Build including user-management

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,58 +1,39 @@
 import { AxiosInstance } from "axios";
-declare const _default: (
-  apiKey: string
-) => {
-  Location: {
-    create: (
-      params: import("./types/location").CreateLocationParams
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    getAllLocations: (
-      params: import("./types/location").GetAllLocationsParams
-    ) => Promise<import("axios").AxiosResponse<any>>;
-  };
-  Interactions: {
-    getByLocationId: (
-      locationId: string,
-      params?: import("./types/interactions").InteractionParams | undefined
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    getById: (
-      intractionByIds?: string[] | undefined
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    respondToAnInteraction: (
-      params: import("./types/interactions").RespondInteraction
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    getSourcesByLocation: (
-      locationId: string
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    addInteractionSource: (
-      params: import("./types/interactions").AddInteractionSource
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    editInteractionSource: (
-      params: import("./types/interactions").InteractionParams
-    ) => Promise<import("axios").AxiosResponse<any>>;
-  };
-  Folders: {
-    addLocations: (
-      params: import("./types/folders").AddLocationsPropierties
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    rename: (
-      params: import("./types/folders").RenameParams
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    removeLocation: (
-      params: import("./types/folders").RemoveLocations
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    deleteFolder: (
-      params: import("./types/folders").DeleteFolder
-    ) => Promise<import("axios").AxiosResponse<any>>;
-  };
-  Tags: {
-    addLocations: (
-      params: import("./types/tags").LocationTag
-    ) => Promise<import("axios").AxiosResponse<any>>;
-    removeLocations: (
-      params: import("./types/tags").LocationTag
-    ) => Promise<import("axios").AxiosResponse<any>>;
-  };
-  _axios: AxiosInstance;
+declare const _default: (apiKey: string) => {
+    Location: {
+        create: (params: import("./types/location").CreateLocationParams) => Promise<import("axios").AxiosResponse<any>>;
+        getAllLocations: (params: import("./types/location").GetAllLocationsParams) => Promise<import("axios").AxiosResponse<any>>;
+    };
+    User: {
+        getAllUsersRoles: (params: import("./types/User").User) => Promise<import("axios").AxiosResponse<any>>;
+        createUserWithRole: (params: import("./types/User").CreateUserWithRole) => Promise<import("axios").AxiosResponse<any>>;
+        addLocationUser: (params: import("./types/User").LocationUser) => Promise<import("axios").AxiosResponse<any>>;
+        addFoldersUser: (params: import("./types/User").FolderUser) => Promise<import("axios").AxiosResponse<any>>;
+        getAllUsers: (params: import("./types/User").User) => Promise<import("axios").AxiosResponse<any>>;
+        listUserResources: (userId: import("./types/User").User) => Promise<import("axios").AxiosResponse<any>>;
+        listUsersDetailsById: (userIds?: string[] | undefined) => Promise<import("axios").AxiosResponse<any>>;
+        updateUser: (params: import("./types/User").User) => Promise<import("axios").AxiosResponse<any>>;
+        removeLocationUser: (params: import("./types/User").LocationUser) => Promise<import("axios").AxiosResponse<any>>;
+        removeFoldersUser: (params: import("./types/User").FolderUser) => Promise<import("axios").AxiosResponse<any>>;
+    };
+    Interactions: {
+        getByLocationId: (locationId: string, params?: import("./types/interactions").InteractionParams | undefined) => Promise<import("axios").AxiosResponse<any>>;
+        getById: (intractionByIds?: string[] | undefined) => Promise<import("axios").AxiosResponse<any>>;
+        respondToAnInteraction: (params: import("./types/interactions").RespondInteraction) => Promise<import("axios").AxiosResponse<any>>;
+        getSourcesByLocation: (locationId: string) => Promise<import("axios").AxiosResponse<any>>;
+        addInteractionSource: (params: import("./types/interactions").AddInteractionSource) => Promise<import("axios").AxiosResponse<any>>;
+        editInteractionSource: (params: import("./types/interactions").InteractionParams) => Promise<import("axios").AxiosResponse<any>>;
+    };
+    Folders: {
+        addLocations: (params: import("./types/folders").AddLocationsPropierties) => Promise<import("axios").AxiosResponse<any>>;
+        rename: (params: import("./types/folders").RenameParams) => Promise<import("axios").AxiosResponse<any>>;
+        removeLocation: (params: import("./types/folders").RemoveLocations) => Promise<import("axios").AxiosResponse<any>>;
+        deleteFolder: (params: import("./types/folders").DeleteFolder) => Promise<import("axios").AxiosResponse<any>>;
+    };
+    Tags: {
+        addLocations: (params: import("./types/tags").LocationTag) => Promise<import("axios").AxiosResponse<any>>;
+        removeLocations: (params: import("./types/tags").LocationTag) => Promise<import("axios").AxiosResponse<any>>;
+    };
+    _axios: AxiosInstance;
 };
 export = _default;
--- dist/index.js
+++ dist/index.js
@@ -2,20 +2,24 @@
 var axios_1 = require("axios");
 var location_service_1 = require("./location.service");
 var user_service_1 = require("./user.service");
+var interaction_service_1 = require("./interaction.service");
+var folders_service_1 = require("./folders.service");
+var tags_service_1 = require("./tags.service");
 var baseURL = "https://api.synup.com/api/v4/";
 var _axios;
 module.exports = function (apiKey) {
-  if (apiKey) {
-    _axios = axios_1.default.create({
-      baseURL: baseURL,
-      headers: { Authorization: "API " + apiKey },
-    });
-  }
-  return {
-    Location: location_service_1.default(_axios),
-    Interactions: interaction_service_1.default(_axios),
-    Folders: folders_service_1.default(_axios),
-    Tags: tags_service_1.default(_axios),
-    _axios: _axios,
-  };
+    if (apiKey) {
+        _axios = axios_1.default.create({
+            baseURL: baseURL,
+            headers: { Authorization: "API " + apiKey },
+        });
+    }
+    return {
+        Location: location_service_1.default(_axios),
+        User: user_service_1.default(_axios),
+        Interactions: interaction_service_1.default(_axios),
+        Folders: folders_service_1.default(_axios),
+        Tags: tags_service_1.default(_axios),
+        _axios: _axios,
+    };
 };
