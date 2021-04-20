f941eeb2f9862a7441f3d982366f39da19c40c76
Elyo <elyo.idesi@gmail.com>
2021-04-20 12:10:11 -0500

    create method in service created

--- src/campaigns.service.ts
+++ src/campaigns.service.ts
@@ -1,7 +1,12 @@
 import{AxiosInstance} from "axios"
+import { Customer } from "./types/campaigns";
+
+const endpoint = "locations/review-campaigns"
 
 export default (axios: AxiosInstance) => {
     return {
-      
+      create:(params:Customer)=>{
+        return axios.post(`${endpoint}`,params)
+      }
     };
   };
