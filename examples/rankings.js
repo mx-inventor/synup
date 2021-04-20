107cffd8c2bef00e213254937c8a39e5e42aa53d
Marisol <Marisol.isic96@gmail.com>
2021-04-20 17:05:21 -0500

    uncommented example lines

--- examples/rankings.js
+++ examples/rankings.js
@@ -2,34 +2,31 @@ const synup = require("../dist/index")(process.env.SYNUP_KEY);
 
 const {Rankings} = synup;
 
-//ADD KEYWORDS
-//  let addKeywords = {
-//      locationId: "TG9jYXRpb246MTM5OTg=",
-//      inputKeywords: ["road paving", "road building"]
-//     }
+  let addKeywords = {
+      locationId: "TG9jYXRpb246MTM5OTg=",
+      inputKeywords: ["road paving", "road building"]
+     }
 
-// Rankings.addKeywordsLocation(addKeywords)
-//    .then((response) => {
-//      console.log(JSON.stringify(response.data));
-//      console.log("SUCCESS");
-//    })
-//    .catch((error) => {
-//      console.log(error);
-//      console.log("FAILURE");
-//  });
+ Rankings.addKeywordsLocation(addKeywords)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      console.log("SUCCESS");
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("FAILURE");
+  });
 
-// //LIST KEYWORDS
-// Rankings.listKeywords()
-// .then((response) => {
-//     console.log(JSON.stringify(response.data));
-//     console.log("SUCCESS");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//     console.log("FAILURE");
-//  });
+ Rankings.listKeywords()
+ .then((response) => {
+     console.log(JSON.stringify(response.data));
+     console.log("SUCCESS");
+   })
+   .catch((error) => {
+     console.log(error);
+     console.log("FAILURE");
+  });
 
-//GET KEYWORDS PERMORMANCE
 let locationId = "TG9jYXRpb246MjE5MjMx";
 
 Rankings.getKeywordsPerformance(locationId)
@@ -42,17 +39,16 @@ Rankings.getKeywordsPerformance(locationId)
     console.log("FAILURE");
   });
 
-//ARCHIVE KEYWORD
-// let archiveKeyword = {
-//     id: "S2V5d29yZDo3NjQzMTE="
-// }
+ let archiveKeyword = {
+     id: "S2V5d29yZDo3NjQzMTE="
+ }
 
-// Rankings.archiveKeyword(archiveKeyword)
-// .then((response) => {
-//     console.log(JSON.stringify(response.data));
-//     console.log("SUCCESS");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//     console.log("FAILURE");
-//   });
+ Rankings.archiveKeyword(archiveKeyword)
+ .then((response) => {
+     console.log(JSON.stringify(response.data));
+     console.log("SUCCESS");
+   })
+   .catch((error) => {
+     console.log(error);
+     console.log("FAILURE");
+   });
