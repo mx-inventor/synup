2d5b3899eee43a457b3289cd527b89dea12ea124
Elyo <elyo.idesi@gmail.com>
2021-04-20 11:35:47 -0500

    location id in example changed for a fake one

--- examples/listings.js
+++ examples/listings.js
@@ -1,7 +1,7 @@
 const synup = require("../dist/index")(process.env.SYNUP_KEY);
 
 const { Listings } = synup;
-let locationId = "TG9jYXRpb246NTYyNjgy"; 
+let locationId = "TG9jYXRpb246MTQwMjQ="; 
 
 Listings.getPremium(locationId)
   .then((response) => {
